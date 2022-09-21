42c8d7e3811a0069d3cdf404a5b6810a75f9bb55
Yutong Yi <YiAndrew2001@gmail.com>
2022-09-21 12:16:17 -0700

    Load possible words into DataFrame

--- wordle_assistant.py
+++ wordle_assistant.py
@@ -7,17 +7,26 @@ https://www.nytimes.com/games/wordle/index.html
 """
 
 
+import pandas as pd
+
+
 class WordleAssistant:
 
     def __init__(self):
         # dictionary of letters that are included in the word
-        # each key is a letter
+        # each key is a single letter
         # each value is a list of two sets:
         # element [0] is the set of identified correct positions (green tiles)
         # element [1] is the set of identified incorrect positions (yellow tiles)
         self.included_letters = {}
         # set of letters that are not included in the word (grey tiles)
         self.excluded_letters = set()
+        # DataFrame of possible words with 5 columns
+        # each column is a single letter
+        # the list of possible words (possible_words.txt) is from 3b1b's GitHub:
+        # https://github.com/3b1b/videos/tree/master/_2022/wordle
+        # where the possible_words.txt is located in the data directory
+        self._possible_words = self._load_possible_words("data\possible_words.txt")
 
 
     def guess(self, word: str, colors: str) -> None:
@@ -39,6 +48,15 @@ class WordleAssistant:
             elif color == "x":
                 if letter not in self.included_letters:
                     self.excluded_letters.add(letter)
+
+
+    @staticmethod
+    def _load_possible_words(path: str) -> pd.DataFrame:
+        with open(path) as f:
+            words = f.readlines()
+            words_split = [list(w.rstrip()) for w in words]
+            words_df = pd.DataFrame(words_split, columns=[0, 1, 2, 3, 4])
+            return words_df
 
 
 if __name__ == "__main__":
